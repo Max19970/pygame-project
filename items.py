3c14f2541381c90f1143880fc53515c5a933a048
maxsh <maxshubert@yandex.ru>
2022-01-12 10:50:25 +0400

    items file

---
+++ items.py
@@ -0,0 +1,74 @@
+items = [
+    {
+        'item_id': 1,
+        'sprite': 'data/theSadOnion.png',
+        'name': 'Грустный лук',
+        'subtext': 'Скорострельность повыш.',
+        'effect': {'fire_rate': ((1 / 2.73) - (1 / 3.45))}
+    },
+    {
+        'item_id': 2,
+        'sprite': 'data/steven.png',
+        'name': 'Стивен',
+        'subtext': 'Урон повыш.',
+        'effect': {'damage': 1}
+    },
+    {
+        'item_id': 3,
+        'sprite': 'data/fate.png',
+        'name': 'Судьба',
+        'subtext': 'Полёт',
+        'effect': {'flight': True}
+    },
+    {
+        'item_id': 4,
+        'sprite': 'data/cupidArrow.png',
+        'name': 'Стрела Купидона',
+        'subtext': 'Пронзающие выстрелы',
+        'effect': {'piercing_shots': True}
+    },
+    {
+        'item_id': 5,
+        'sprite': 'data/ouijaBoard.png',
+        'name': 'Спиритическая доска',
+        'subtext': 'Спектральные выстрелы',
+        'effect': {'spectral_shots': True}
+    },
+    {
+        'item_id': 6,
+        'sprite': 'data/growthHormones.png',
+        'name': 'Гормоны роста',
+        'subtext': 'Урон + скорость повыш.',
+        'effect': {'damage': 1,
+                   'speed': 0.2}
+    },
+    {
+        'item_id': 7,
+        'sprite': 'data/breakfast.png',
+        'name': 'Завтрак',
+        'subtext': 'Здоровье повыш., Скорость пониж.',
+        'effect': {'hp': 2,
+                   'speed': -0.2}
+    },
+    {
+        'item_id': 8,
+        'sprite': 'data/infamy.png',
+        'name': 'Позор',
+        'subtext': 'Уменьшение повреждений',
+        'effect': {'damage_block_chance': 0.2}
+    },
+    {
+        'item_id': 9,
+        'sprite': 'data/magicMeat.png',
+        'name': 'Волшебное мясо',
+        'subtext': 'Здоровье повыш.',
+        'effect': {'hp': 2}
+    },
+    {
+        'item_id': 10,
+        'sprite': 'data/speedBall.png',
+        'name': 'Спидбол',
+        'subtext': 'Скорость + скорость выстрела повыш.',
+        'effect': {'speed': 0.3,
+                   'shot_speed': 0.16}
+    }]
