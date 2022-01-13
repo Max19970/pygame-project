3dbce273bcd481a2203b9f2e12d18e7e461b10ee
alexm <alexmercer2307@gmail.com>
2022-01-13 15:34:05 +0400

    "стрельба"

--- igra.py
+++ igra.py
@@ -142,9 +142,51 @@ def generate_level(level):
     # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = 1
+        self.vy = 1
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
+
+
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+all_sprites = pygame.sprite.Group()
+size = width, height = 850, 350
+screen = pygame.display.set_mode(size)
+
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+Border(5, 5, width - 5, 5)
+Border(5, height - 5, width - 5, height - 5)
+Border(5, 5, 5, height - 5)
+Border(width - 5, 5, width - 5, height - 5)
 
 if __name__ == '__main__':
-    size = WIDTH, HEIGHT = 850, 350
     screen = pygame.display.set_mode(size)
     pygame.init()
     pygame.display.init()
@@ -179,12 +221,16 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     player.move('up', level_map)
+                    Ball(10, 425, 175)
                 elif event.key == pygame.K_RIGHT:
                     player.move('right', level_map)
+                    Ball(10, 425, 175)
                 elif event.key == pygame.K_DOWN:
                     player.move('down', level_map)
+                    Ball(10, 425, 175)
                 elif event.key == pygame.K_LEFT:
                     player.move('left', level_map)
+                    Ball(10, 425, 175)
         screen.fill((0, 0, 0))
         # camera.update(player)
         # for sprite in all_sprites:
