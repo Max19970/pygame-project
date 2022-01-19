19cc33d09eed88af4fd4997a309fc9f3e9e718ff
Max <77971649+Max19970@users.noreply.github.com>
2022-01-19 22:33:28 +0400

    Update
    Птицы и рандомная генерация препятствий

--- characters.py
+++ characters.py
@@ -1,14 +1,15 @@
 import pygame
 import os
 import sys
-import main
 
 
 all_sprites = pygame.sprite.Group()
 player_sprites = pygame.sprite.Group()
 bird_sprites = pygame.sprite.Group()
 cactus_sprites = pygame.sprite.Group()
+ground_sprites = pygame.sprite.Group()
 flag = False
+
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -65,62 +66,86 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(50, 253)
 
 
-
 class Ground(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, ground_sprites)
         self.image = load_image('ground.png')
         self.rect = self.image.get_rect().move(x, y)
 
-    def move(self):
-        self.rect.x -= 5
+    def move(self, speedgo):
+        self.rect.x -= speedgo
 
     def spawn(self):
         self.kill()
         self.rect = self.image.get_rect().move(0, 330)
 
 
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(bird_sprites, all_sprites)
+        self.sprites = self.cut_sprites(load_image('ptera.png'))
+        self.image = self.sprites[0]
+        self.rect = self.image.get_rect().move(x, y)
 
+    def cut_sprites(self, sprites_file):
+        sprites_cut = []
+        for i in range(1):
+            for j in range(2):
+                sprite_location = 92 * j, 81 * i
+                sprites_cut.append(sprites_file.subsurface(pygame.Rect(
+                    sprite_location, (92, 81)
+                )))
+        return sprites_cut
 
-#class Bird(pygame.sprite.Sprite):
-    #def __init__(self):
-        #super().__init__(bird_sprites, all_sprites)
+    def move(self, speedgo):
+        self.rect.x -= speedgo
+
+    def spawn(self, x):
+        self.rect = self.image.get_rect().move(x, 200)
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, player):
+        if pygame.sprite.collide_mask(self, player):
+            global flag
+            flag = True
+
 
 class Cactus(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, cactus_sprites)
         self.image = load_image('cactus.png')
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(800, 253)
 
-    def move(self):
-        self.rect.x -= 5
+    def move(self, speedgo):
+        self.rect.x -= speedgo
+
     def spawn(self, x):
         self.rect = self.image.get_rect().move(x, 253)
         self.mask = pygame.mask.from_surface(self.image)
 
-    def update(self):
-        if pygame.sprite.collide_mask(self, Player()):
+    def update(self, player):
+        if pygame.sprite.collide_mask(self, player):
             global flag
             flag = True
 
 
 class cactusm(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, cactus_sprites)
         self.image = load_image('cacti-small.png')
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(x, 273)
-    def move(self):
-        self.rect.x -= 5
+    def move(self, speedgo):
+        self.rect.x -= speedgo
 
     def spawn(self, x):
         self.rect = self.image.get_rect().move(x, 273)
         self.mask = pygame.mask.from_surface(self.image)
 
 
-    def update(self):
-        if pygame.sprite.collide_mask(self, Player()):
+    def update(self, player):
+        if pygame.sprite.collide_mask(self, player):
             global flag
             flag = True
 
@@ -128,58 +153,59 @@ class cactusm(pygame.sprite.Sprite):
 
 class Cactus3(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, cactus_sprites)
         self.image = load_image('cacti-3.png')
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(x, 253)
 
-    def move(self):
-        self.rect.x -= 5
+    def move(self, speedgo):
+        self.rect.x -= speedgo
     def spawn(self, x):
         self.rect = self.image.get_rect().move(x, 253)
         self.mask = pygame.mask.from_surface(self.image)
 
 
-    def update(self):
-        if pygame.sprite.collide_mask(self, Player()):
+    def update(self, player):
+        if pygame.sprite.collide_mask(self, player):
             global flag
             flag = True
 
 class cactusm2(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, cactus_sprites)
         self.image = load_image('cacti-small2.png')
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(x, 273)
-    def move(self):
-        self.rect.x -= 5
+    def move(self, speedgo):
+        self.rect.x -= speedgo
 
     def spawn(self, x):
         self.rect = self.image.get_rect().move(x, 273)
         self.mask = pygame.mask.from_surface(self.image)
 
 
-    def update(self):
-        if pygame.sprite.collide_mask(self, Player()):
+    def update(self, player):
+        if pygame.sprite.collide_mask(self, player):
             global flag
             flag = True
 
 class Cactus4(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__(all_sprites)
+        super().__init__(all_sprites, cactus_sprites)
         self.image = load_image('cacti-2.png')
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect().move(x, 2530)
 
-    def move(self):
-        self.rect.x -= 5
-    def spawn(self):
-        self.rect = self.image.get_rect().move(2280, 253)
+    def move(self, speedgo):
+        self.rect.x -= speedgo
+
+    def spawn(self, x):
+        self.rect = self.image.get_rect().move(x, 253)
         self.mask = pygame.mask.from_surface(self.image)
 
 
-    def update(self):
-        if pygame.sprite.collide_mask(self, Player()):
+    def update(self, player):
+        if pygame.sprite.collide_mask(self, player):
             global flag
             flag = True
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import characters
 import start_screen
-from random import randint
+from random import randint, choice
 
 
 def main():
@@ -24,17 +24,22 @@ def main():
     cactus3 = characters.Cactus3(randint(1500, 1700), 275)
     cactus4 = characters.Cactus4(randint(500, 1200), 275)
     cactusm2 = characters.cactusm2(randint(1850, 2050), 275)
+    bird = characters.Bird(randint(2200, 2300), 200)
     ground_further = characters.Ground(ground.rect.size[0], 330)
     speed = 0
     score = 0
+    i = 0
+    speedgo = 5
     started = False
     player_jumping = False
     player_duck = False
+    dont_move = False
 
     player_jump = pygame.USEREVENT + 1
     player_sprite_change = pygame.USEREVENT + 2
     ground_move = pygame.USEREVENT + 3
     score_count = pygame.USEREVENT + 4
+    damage = pygame.USEREVENT + 5
     pygame.time.set_timer(player_sprite_change, 100)
     pygame.time.set_timer(ground_move, 10)
     pygame.time.set_timer(score_count, 100)
@@ -56,7 +61,7 @@ def main():
                         speed = 12
                         pygame.time.set_timer(player_jump, 10)
                         player_jumping = True
-                    elif event.key == pygame.K_DOWN and not player_jumping:
+                    elif event.key == pygame.K_DOWN:
                         player_duck = True
                         player.image = player.duck_sprites[0]
                 elif event.type == pygame.KEYUP:
@@ -65,8 +70,11 @@ def main():
                         player.image = player.sprites[2]
                 elif event.type == player_jump:
                     player.jump(speed)
-                    speed -= 0.5
-                elif event.type == player_sprite_change:
+                    if player_duck:
+                        speed -= 1
+                    else:
+                        speed -= 0.5
+                elif event.type == player_sprite_change and not dont_move:
                     if not player_duck:
                         if player.image == player.sprites[2]:
                             player.image = player.sprites[3]
@@ -77,55 +85,135 @@ def main():
                             player.image = player.duck_sprites[1]
                         else:
                             player.image = player.duck_sprites[0]
-                elif event.type == ground_move:
-                    ground.move()
-                    ground_further.move()
-                    cactus.move()
-                    cactusm.move()
-                    cactus3.move()
-                    cactusm2.move()
+                    if bird.image == bird.sprites[0]:
+                        bird.image = bird.sprites[1]
+                    else:
+                        bird.image = bird.sprites[0]
+                elif event.type == ground_move and not dont_move:
+                    ground.move(speedgo)
+                    ground_further.move(speedgo)
+                    cactus.move(speedgo)
+                    cactusm.move(speedgo)
+                    cactus3.move(speedgo)
+                    cactusm2.move(speedgo)
+                    bird.move(speedgo)
                     if ground.rect.x + ground.rect.size[0] < 0:
                         ground.rect.x = ground_further.rect.size[0]
-                        cactusm.spawn(randint(800,900))
-                        cactus.spawn(randint(1100, 1300))
-                        cactus3.spawn(randint(1500, 1700))
-                        cactusm2.spawn(randint(1850, 2050))
-                        cactus4.spawn()
+                        xs = [randint(800, 1000) + 20 * speedgo,
+                              randint(1200, 1400) + 20 * speedgo,
+                              randint(1600, 1800) + 20 * speedgo,
+                              randint(2000, 2200) + 20 * speedgo,
+                              randint(2400, 2600) + 20 * speedgo,
+                              randint(2800, 3000) + 20 * speedgo]
+                        cactusmx = choice(xs)
+                        del xs[xs.index(cactusmx)]
+                        cactusx = choice(xs)
+                        del xs[xs.index(cactusx)]
+                        cactus3x = choice(xs)
+                        del xs[xs.index(cactus3x)]
+                        cactusm2x = choice(xs)
+                        del xs[xs.index(cactusm2x)]
+                        birdx = choice(xs)
+                        del xs[xs.index(birdx)]
+                        cactus4x = choice(xs)
+                        del xs[xs.index(cactus4x)]
+                        spawns = sorted([(cactusmx, 'cactusm', cactusm),
+                                         (cactusx, 'cactus', cactus),
+                                         (cactus3x, 'cactus3', cactus3),
+                                         (cactusm2x, 'cactusm2', cactusm2),
+                                         (birdx, 'bird', bird),
+                                         (cactus4x, 'cactus4', cactus4)], key=lambda x: x[0])
+                        for e in spawns:
+                            if e[2].rect.x + e[2].rect.size[0] < 0:
+                                exec(f'{e[1]}.spawn({e[0]})')
                     elif ground_further.rect.x + ground_further.rect.size[0] < 0:
                         ground_further.rect.x = ground.rect.size[0]
-                        cactusm.spawn(randint(1500, 1700))
-                        cactus.spawn(randint(1850, 2050))
-                        cactus3.spawn(randint(1100, 1300))
-                        cactusm2.spawn(randint(800,801))
-                        cactus4.spawn()
-
-                elif event.type == score_count:
+                        xs = [randint(800, 1000) + 20 * speedgo,
+                              randint(1200, 1400) + 20 * speedgo,
+                              randint(1600, 1800) + 20 * speedgo,
+                              randint(2000, 2200) + 20 * speedgo,
+                              randint(2400, 2600) + 20 * speedgo,
+                              randint(2800, 3000) + 20 * speedgo]
+                        cactusmx = choice(xs)
+                        del xs[xs.index(cactusmx)]
+                        cactusx = choice(xs)
+                        del xs[xs.index(cactusx)]
+                        cactus3x = choice(xs)
+                        del xs[xs.index(cactus3x)]
+                        cactusm2x = choice(xs)
+                        del xs[xs.index(cactusm2x)]
+                        birdx = choice(xs)
+                        del xs[xs.index(birdx)]
+                        cactus4x = choice(xs)
+                        del xs[xs.index(cactus4x)]
+                        spawns = sorted([(cactusmx, 'cactusm', cactusm),
+                                         (cactusx, 'cactus', cactus),
+                                         (cactus3x, 'cactus3', cactus3),
+                                         (cactusm2x, 'cactusm2', cactusm2),
+                                         (birdx, 'bird', bird),
+                                         (cactus4x, 'cactus4', cactus4)], key=lambda x: x[0])
+                        for e in spawns:
+                            if e[2].rect.x + e[2].rect.size[0] < 0:
+                                exec(f'{e[1]}.spawn({e[0]})')
+                elif event.type == score_count and not dont_move:
                     score += 1
-                if characters.stolknovenie() == True and player_jumping != True:
-                    cactusm.spawn(randint(1500, 1700))
-                    cactus.spawn(randint(1850, 2050))
-                    cactus3.spawn(randint(1100, 1300))
-                    cactusm2.spawn(randint(800, 801))
-                    cactus4.spawn()
+                    if score % 100 == 0:
+                        speedgo += 1
+                if characters.stolknovenie() and not dont_move:
+                    dont_move = True
+                    player.image = player.sprites[4]
+                    screen.fill(background)
+                    characters.ground_sprites.draw(screen)
+                    characters.ground_sprites.update()
+                    characters.cactus_sprites.draw(screen)
+                    characters.cactus_sprites.update(player)
+                    characters.player_sprites.draw(screen)
+                    characters.player_sprites.update()
+                    characters.bird_sprites.draw(screen)
+                    characters.bird_sprites.update(player)
+                    score_to_blit = pygame.font.Font(None, 50).render(f'{score}', True, pygame.Color('black'))
+                    screen.blit(score_to_blit, (30, 50, 100, 100))
+                    pygame.time.set_timer(damage, 2000)
+                elif event.type == damage:
+                    pygame.time.set_timer(damage, 0)
+                    ground.spawn()
+                    ground_further.spawn()
+                    cactus.kill()
+                    cactusm.kill()
+                    cactus3.kill()
+                    cactus4.kill()
+                    cactusm2.kill()
+                    bird.kill()
+                    cactus.spawn(randint(500, 1200))
+                    cactusm.spawn(randint(1100, 1300))
+                    cactus3.spawn(randint(1500, 1700))
+                    cactus4.spawn(randint(500, 1200))
+                    cactusm2.spawn(randint(1850, 2050))
+                    bird.spawn(randint(2200, 2300))
                     player.spawn()
+                    # !!! экран проигрыша СЮДА !!!
                     main()
 
-
-
-
-
-
-        if speed < -12.5:
-            pygame.time.set_timer(player_jump, 0)
-            player_jumping = False
-        screen.fill(background)
-        characters.all_sprites.draw(screen)
-        characters.all_sprites.update()
+        if not dont_move:
+            if speed < -12.5 and player.rect.y >= 253:
+                pygame.time.set_timer(player_jump, 0)
+                player_jumping = False
+            screen.fill(background)
+            characters.ground_sprites.draw(screen)
+            characters.ground_sprites.update()
+            characters.cactus_sprites.draw(screen)
+            characters.cactus_sprites.update(player)
+            characters.player_sprites.draw(screen)
+            characters.player_sprites.update()
+            characters.bird_sprites.draw(screen)
+            characters.bird_sprites.update(player)
+            score_to_blit = pygame.font.Font(None, 50).render(f'{score}', True, pygame.Color('black'))
+            screen.blit(score_to_blit, (30, 50, 100, 100))
+        else:
+            player.image = player.sprites[4]
         if not started:
             screen.blit(pygame.font.Font(None, 50).render('Нажмите Enter, чтобы начать',
                                                           True, pygame.Color('black')), (160, 180, 300, 300))
-        score_to_blit = pygame.font.Font(None, 50).render(f'{score}', True, pygame.Color('black'))
-        screen.blit(score_to_blit, (30, 50, 100, 100))
         pygame.display.flip()
         clock.tick(60)
 
